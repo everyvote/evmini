c67ce175c0c7a83cf94019d5c62fdc815469bf8c
Peter Brenner <peter@phpwerks.com>
2013-02-25 20:50:22 -0500

    Issue #11
    Removed term_start and term_end
    
    Signed-off-by: Peter Brenner <peter@phpwerks.com>

--- app/Model/Office.php
+++ app/Model/Office.php
@@ -34,26 +34,6 @@ class Office extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
-		'term_start' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
-		'term_end' => array(
-			'datetime' => array(
-				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
